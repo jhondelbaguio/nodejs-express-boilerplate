da235340669ded1a939605c7146f672af398d40d
Jhondel <jhondel.louis@gmail.com>
2016-03-22 12:10:32 +0800

    Change env init config

--- app/config/init.js
+++ app/config/init.js
@@ -1,10 +1,25 @@
 /**
  * Initialize Node Environment
  */
-
-
 'use strict';
 
+var glob = require('glob'),
+    dotenv = require('dotenv'),
+    chalk = require('chalk');
+
+dotenv.load();
+
 module.exports = function() {
-    process.env.NODE_ENV = 'development'
+
+    var envFiles = glob('./app/config/env/' + process.env.NODE_ENV + '.js', { sync: true });
+
+    if (!envFiles.length) {
+        if (process.env.NODE_ENV) {
+            console.error(chalk.red('No configuration file found for "' + process.env.NODE_ENV + '" environment using development instead'));
+        } else {
+            console.error(chalk.red('NODE_ENV is not defined! Using default development environment'));
+        }
+    } else {
+        console.log(chalk.black.bgWhite('Application loaded using the "' + process.env.NODE_ENV + '" environment configuration'));
+    }
 }
